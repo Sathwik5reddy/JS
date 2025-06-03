49852fb1910e914e4ce6e99b21917d6fb4fdddf6
Sathwik Reddy <144688765+Sathwik5reddy@users.noreply.github.com>
2025-06-03 06:38:33 +0000

    DataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3 + 3)
+// code readability should be high
+console.log("Sathwik")
+
+let name = "Sathwik"
+let age = 23
+let isLoggedIn = true
+let state; //undefined
+let sno = null // null
+
+// number => 2 to power of 53
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined => value is not assigned
+// symbol => unique 
+
+
+// object
+
+console.log(typeof "sathwik"); //string
+console.log(typeof age); //number
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined
